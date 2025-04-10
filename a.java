1fdf550e2aabccf4f8c5634977e72e0ea3095983
Chandana i k <82880993+Chandanaik@users.noreply.github.com>
2025-04-10 19:15:38 +0530

    Update a.java

--- a.java
+++ a.java
@@ -15,9 +15,7 @@ public class ArrayReverse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int arr[]={1,2,3,4,5};
-       for(int i=arr.length-1;i>=0;i--){
-       System.out.print(arr[i]+" ");
+        System.out.println("Hello world!");
     }
 }
     
